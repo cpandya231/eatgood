bd6860a846c2ad493120ba6e4f33e30325190459
u8zfvf <chintan.pandya@siemens.com>
2020-03-26 17:59:29 +0530

    Resolve cors issue

--- controller/restaurantController.js
+++ controller/restaurantController.js
@@ -6,12 +6,22 @@ var router = express.Router();
 
 router.get("/", (req, res) => {
     let city = req.query.city;
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
     scanDDB.getRestaurant((err, data) => {
         if (err) {
-            console.log("No data found in restaurant table");
             res.json(err);
         } else {
-            console.log("Data found");
             res.json(data);
         }
     });
