fb1d77c24f8b36a1c1fc477128afbea05cbab423
u8zfvf <chintan.pandya@siemens.com>
2020-04-13 18:19:34 +0530

    Replaced callback function with promise

--- controller/restaurantController.js
+++ controller/restaurantController.js
@@ -19,12 +19,12 @@ router.get("/", (req, res) => {
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-    scanDDB.getRestaurant(queryParams, (err, data) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(data);
-        }
+    scanDDB.getRestaurant(queryParams).then((data) => {
+
+        res.json(data);
+
+    }).catch((err) => {
+        res.json(err);
     });
 
 })
--- model/scanItemDDB.js
+++ model/scanItemDDB.js
@@ -12,47 +12,49 @@ console.log("Querying for restaurants");
 
 
 
-function getRestaurant(queryParams, callback) {
-    var params = {
-        TableName: "restaurant"
-    };
-    if (queryParams.city) {
-        var expressionValues = {
-
-            ":City": queryParams.city,
-            ":RestaurantID": parseInt(queryParams.restaurantID)
+function getRestaurant(queryParams) {
+    return new Promise((resolve, reject) => {
+        var params = {
+            TableName: "restaurant"
         };
-        params.ExpressionAttributeValues = expressionValues;
-        params.KeyConditionExpression = "#City= :City and #RestaurantID= :RestaurantID";
-        params.ExpressionAttributeNames = {
-            "#City": "City",
-            "#RestaurantID": "Restaurant ID"
+        if (queryParams.city) {
+            var expressionValues = {
 
-        };
+                ":City": queryParams.city,
+                ":RestaurantID": parseInt(queryParams.restaurantID)
+            };
+            params.ExpressionAttributeValues = expressionValues;
+            params.KeyConditionExpression = "#City= :City and #RestaurantID= :RestaurantID";
+            params.ExpressionAttributeNames = {
+                "#City": "City",
+                "#RestaurantID": "Restaurant ID"
 
-        docClient.query(params, function(err, data) {
-            if (err) {
-                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-                callback(err, null);
-            } else {
-                console.log("Query succeeded.");
-                callback(null, data.Items);
+            };
 
-            }
-        });
-    } else {
-        docClient.scan(params, function(err, data) {
-            if (err) {
-                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-                callback(err, null);
-            } else {
-                console.log("Query succeeded.");
-                callback(null, data.Items);
+            docClient.query(params, function(err, data) {
+                if (err) {
+                    console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+                    reject(err);
+                } else {
+                    console.log("Query succeeded.");
+                    resolve(data.Items);
 
-            }
-        });
-    }
+                }
+            });
+        } else {
+            docClient.scan(params, function(err, data) {
+                if (err) {
+                    console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+                    reject(err);
+                } else {
+                    console.log("Query succeeded.");
+                    resolve(data.Items);
 
+                }
+            });
+        }
+
+    });
 
 
 
